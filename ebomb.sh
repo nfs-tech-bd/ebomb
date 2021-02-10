66644829d3078bc06a0c4516a4653a324970bbb2
B14CK-KN1GH7 <78839557+nfs-tech-bd@users.noreply.github.com>
2021-02-10 15:40:14 +0600

    Add files via upload

---
+++ ebomb.sh
@@ -0,0 +1,7 @@
+clear
+#figlet -f ASCII-Shadow EMS
+#echo CODED BY B14CK-KN1GH7 (NAFIS)
+#echo FACEBOOK : http://facebook.com/nafis.fuad.904
+#echo WEBSITE : http://entertainbd.wapkiz.com
+python2 .logo.py | lolcat --animate
+python2 .ebomb.py
