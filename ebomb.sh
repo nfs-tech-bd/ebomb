5b623557b4c7cdf859acecc2be41959fd17257e6
B14CK-KN1GH7 <78839557+nfs-tech-bd@users.noreply.github.com>
2021-04-22 10:40:05 +0000

    Delete ebomb.sh

--- ebomb.sh
+++
@@ -1,7 +0,0 @@
-clear
-#figlet -f ASCII-Shadow EMS
-#echo CODED BY B14CK-KN1GH7 (NAFIS)
-#echo FACEBOOK : http://facebook.com/nafis.fuad.904
-#echo WEBSITE : http://entertainbd.wapkiz.com
-python2 .logo.py | lolcat --animate
-python2 .ebomb.py
